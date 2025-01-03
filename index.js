007c0d378d4a5a667dedf8a4b00a233e9137d4e3
rumman321 <cset-210652@dti.ac>
2025-01-03 19:04:25 +0600

    get seleted cart data and delelte single cart

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhac7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +27,37 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+    const database = client.db("BristoDB");
+    const menuCollection = database.collection("menu");
+    const reviewCollection = database.collection("reviews");
+    const cartCollection = database.collection("carts");
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.get("/menu",async (req,res)=>{
+        const result= await menuCollection.find().toArray()
+        res.send(result)
+    })
+    app.get("/reviews",async (req,res)=>{
+        const result= await reviewCollection.find().toArray()
+        res.send(result)
+    })
+    app.post("/carts", async (req,res)=>{
+      const cartItem= req.body
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
+    app.get("/carts",async (req,res)=>{
+      const email = req.query.email
+      const query = {email:email}
+      const result= await cartCollection.find(query).toArray()
+      res.send(result)
+    })  
+    app.delete("/carts/:id", async(req, res)=>{
+      const id= req.params.id
+      const query={_id : new ObjectId(id)}
+      const result = await(cartCollection.deleteOne(query))
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
