58ee10fca4cb9c21cbf9256aee1024054f8c0559
rumman321 <cset-210652@dti.ac>
2025-01-09 18:55:04 +0600

    complete add item and delete item

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
         next();
       });
     };
-    //verify admin aftr verifyToken
+    //verify admin after verifyToken
     const verifyAdmin = async(req, res, next)=>{
       const email = req.decoded.email
       const query ={ email : email}
@@ -119,10 +119,29 @@ async function run() {
       };
       const result = await userCollection.updateOne(filter, updatedDoc);
     });
+    //menu apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    app.get("/menu/:id", async (req, res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await menuCollection.findOne(query)
+      res.send(result)
+    })
+    app.post("/menu", verifyToken, verifyAdmin,  async(req,res)=>{
+      const item = req.body
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+
+    })
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
