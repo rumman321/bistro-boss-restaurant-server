bbdb79ed669865744f07d55e05e07a1a044ca595
rumman321 <cset-210652@dti.ac>
2025-01-14 14:55:46 +0600

    admin-stats and order stats complete

--- index.js
+++ index.js
@@ -212,6 +212,76 @@ async function run() {
       const deleteResult = await cartCollection.deleteMany(query)
       res.send({paymentResult, deleteResult})
     })
+    app.get("/payments/:email", verifyToken, async(req,res)=>{
+      const query = {email : req.params.email}
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message : "forbidden access"})
+      }
+      const result = await paymentCollection.find(query).toArray()
+      res.send(result)
+    })
+    //stats or analytics
+    app.get('/admin-stats',verifyToken, verifyAdmin, async(req, res)=>{
+      const users = await userCollection.estimatedDocumentCount()
+      const menuItems = await menuCollection.estimatedDocumentCount()
+      const orders = await paymentCollection.estimatedDocumentCount()
+      
+      //this is not the best way
+      // const payments = await paymentCollection.find().toArray()
+      // const revenue = payments.reduce((total, payment )=> total + payment.price,0)
+      const result = await paymentCollection.aggregate([
+        {
+          $group:{
+            _id:null,
+            totalRevenue:{$sum: "$price"}
+          }
+        }
+      ]).toArray()
+      const revenue = result[0]?.totalRevenue || 0;
+
+      res.send({
+        users,
+        menuItems,
+        orders, 
+        revenue
+      })
+    })
+    app.get('/order-stats', async(req,res)=>{
+      const result = await paymentCollection.aggregate([
+        {
+          $unwind : '$menuItemIds'
+        },
+        {
+          $lookup:{
+            from:'menu',
+            localField: 'menuItems',
+            foreignField: '_id',
+            as: 'menuItems'
+          }
+        },
+        {
+          $unwind:'$menuItems'
+        },
+        {
+          $group:{
+            _id:'$menuItems.category',
+            quantity:{$sum:1},
+            revenue: {$sum:'$menuItems.price'}
+              
+          }
+        },
+        {
+          $project:{
+            _id: 0,
+            category:'$_id',
+            quantity:'$quantity',
+            revenue:'$revenue'
+          }
+        }
+      ]).toArray();
+      res.send(result)
+    })
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
